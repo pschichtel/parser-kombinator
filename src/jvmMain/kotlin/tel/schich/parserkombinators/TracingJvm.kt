e85e5fd9a1f02563d19f75d462b96464a66667bf
Phillip Schichtel <phillip@schich.tel>
2022-10-07 14:36:14 +0200

    nothing to see here

--- src/jvmMain/kotlin/tel/schich/parserkombinators/TracingJvm.kt
+++ src/jvmMain/kotlin/tel/schich/parserkombinators/TracingJvm.kt
@@ -1,5 +1,5 @@
 package tel.schich.parserkombinators
 
-private val enableTracing = System.getProperty("tel.schich.rfc5988.parsing.trace", "false").toBoolean()
+private val enableTracing = System.getProperty("tel.schich.parser-kombinator.trace", "false").toBoolean()
 
 actual fun isTracingEnabled(): Boolean = enableTracing
